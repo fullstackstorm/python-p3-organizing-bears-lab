acb0c9632a92fcb88c722f2212dc6a85aa186f99
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-07-31 00:01:30 -0700

    Query the database.

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 5, 'Male', 'Brown', 'Calm', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 1, 'Male', 'Black', 'Noisy and Disorderly', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 8, 'Female', 'Grey', 'Sweet', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 5, 'Male', 'Brown', 'Grumpy', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 3, 'Female', 'Black', 'Dainty', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 5, 'Male', 'White', 'Grumpy', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 10, 'Female', 'White', 'Calm', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 5, 'Male', 'Brown', 'Calm', true);
