5ae5fade588b5dce0533a34845075685e0e47ae8
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-07-31 00:21:02 -0700

    Have a friendlier insert statement.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,23 +1,23 @@
 INSERT INTO bears (name, age, sex, color, temperament, alive)
-VALUES ('Mr. Chocolate', 5, 'Male', 'Brown', 'Calm', true);
+VALUES ('Mr. Chocolate', 5, 'Male', 'Brown', 'Calm', 1);
 
 INSERT INTO bears (name, age, sex, color, temperament, alive)
-VALUES ('Rowdy', 1, 'Male', 'Black', 'Noisy and Disorderly', true);
+VALUES ('Rowdy', 1, 'Male', 'Black', 'Noisy and Disorderly', 1);
 
 INSERT INTO bears (name, age, sex, color, temperament, alive)
-VALUES ('Tabitha', 8, 'Female', 'Grey', 'Sweet', true);
+VALUES ('Tabitha', 8, 'Female', 'Grey', 'Sweet', 1);
 
 INSERT INTO bears (name, age, sex, color, temperament, alive)
-VALUES ('Sergeant Brown', 5, 'Male', 'Brown', 'Grumpy', true);
+VALUES ('Sergeant Brown', 5, 'Male', 'Brown', 'Grumpy', 1);
 
 INSERT INTO bears (name, age, sex, color, temperament, alive)
-VALUES ('Melissa', 3, 'Female', 'Black', 'Dainty', true);
+VALUES ('Melissa', 3, 'Female', 'Black', 'Dainty', 1);
 
 INSERT INTO bears (name, age, sex, color, temperament, alive)
-VALUES ('Grinch', 5, 'Male', 'White', 'Grumpy', true);
+VALUES ('Grinch', 5, 'Male', 'White', 'Grumpy', 1);
 
 INSERT INTO bears (name, age, sex, color, temperament, alive)
-VALUES ('Wendy', 10, 'Female', 'White', 'Calm', true);
+VALUES ('Wendy', 10, 'Female', 'White', 'Calm', 1);
 
 INSERT INTO bears (name, age, sex, color, temperament, alive)
-VALUES (NULL, 5, 'Male', 'Brown', 'Calm', true);
+VALUES (NULL, 5, 'Male', 'Brown', 'Calm', 1);
